27e291500d8e8d16cc5f4dfd207844bdb63bb962
umairsolangi <51870556+umairsolangi@users.noreply.github.com>
2020-07-04 21:46:36 +0500

    add smart calculator web application

---
+++ app.js
@@ -0,0 +1,90 @@
+// function getnum(num){
+//     var result = document.getElementById("result")
+//    result.value  += (num)
+// }
+
+
+// function clearResult(){
+//     var result = document.getElementById("result")
+//     result.value = ""
+// }
+
+// function getResult(){
+//     var result = document.getElementById("result")
+//     result.value = eval(result.value)
+// }
+
+
+
+
+
+
+
+
+
+
+
+// function showNumberInInput(num){
+//     var result = document.getElementById("result")
+//     result.value += (num)
+// }
+
+// function clearValue(){
+//     var result = document.getElementById("result")
+//     result.value = ""
+// }
+// function applyFunction(){
+//     var result =document.getElementById("result")
+//     result.value = eval(result.value)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getNumInInput(num){
+    var value = document.getElementById("value")
+    value.value += (num)
+}
+
+function clearValue(){
+    var value = document.getElementById("value")
+    value.value = ""
+}
+function working(){
+    var value = document.getElementById("value")
+    value.value = eval(value.value)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
